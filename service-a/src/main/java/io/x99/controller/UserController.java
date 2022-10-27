aaeb1605e47037e49a21cb297da3728af869fd8d
Rattapong K <rattapongk@99x.io>
2022-10-27 14:40:09 +0700

    add service to handle business logic and return error response when error occurred

--- service-a/src/main/java/io/x99/controller/UserController.java
+++ service-a/src/main/java/io/x99/controller/UserController.java
@@ -1,8 +1,14 @@
 package io.x99.controller;
 
+import io.x99.error.ErrorResponse;
 import io.x99.error.NotFoundException;
 import io.x99.model.UserEntity;
-import io.x99.repository.UserRepository;
+import io.x99.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,21 +17,33 @@ import java.util.List;
 
 @RestController
 public class UserController {
-    private final UserRepository repository;
+    private final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
 
-    UserController(UserRepository repository) {
-        this.repository = repository;
-    }
+    @Autowired
+    private UserService userService;
 
 
     @GetMapping("/user")
-    List<UserEntity> all() {
-        return repository.findAll();
+    ResponseEntity<?> all() {
+        try {
+            List<UserEntity> users = userService.getAllUsers();
+            return new ResponseEntity<>(users, HttpStatus.OK);
+        } catch (Throwable th) {
+            LOGGER.error("Internal Error", th);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @GetMapping("/user/{id}")
-    UserEntity one(@PathVariable Integer id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new NotFoundException("Id is not found"));
+    ResponseEntity<?> one(@PathVariable Integer id) {
+        try {
+            UserEntity user = userService.getUserById(id);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        } catch (NotFoundException e) {
+            return new ResponseEntity<>(new ErrorResponse(e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (Throwable th) {
+            LOGGER.error("Internal Error", th);
+            return new ResponseEntity<>(new ErrorResponse("Internal Error"),HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
---
+++ service-a/src/main/java/io/x99/error/ErrorResponse.java
@@ -0,0 +1,18 @@
+package io.x99.error;
+
+public class ErrorResponse {
+
+    private String error;
+
+    public ErrorResponse(String error) {
+        this.error = error;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+}
