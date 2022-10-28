007ef500815ff073e8b7781a3463983b011ad7b0
aofxzuza <aof.kem@gmail.com>
2022-10-28 18:52:51 +0700

    update service endpoints and class properties (#8)
    Co-authored-by: Rattapong K <rattapongk@99x.io>

---
+++ service-b/src/main/java/io/x99/model/User.java
@@ -0,0 +1,35 @@
+package io.x99.model;
+
+public class User {
+    private Long id;
+    private String name;
+    private String country;
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
